a96827ee8d36e851767e2182fd5461cfea03225d
ana27clara06 <4n4clara@gmail.com>
2023-10-09 16:06:27 -0300

    anaclara

---
+++ anaclara.py
@@ -0,0 +1,17 @@
+dia1= "domingo"
+dia2= "segunda-feira"
+dia3= "terça-feira"
+dia4= "quarta-feira"
+dia5= "quinta-feira"
+dia6= "sexta-feira"
+dia7= "sábado"
+
+
+print (dia1)
+print (dia2)
+print (dia3)
+print (dia4)
+print (dia5)
+print (dia6)
+print (dia7)
+    
